072058e0d3f3bce36f802b26b869eb64638d68fc
malith94batugedara <malithbatugedara@gmail.com>
2024-06-17 17:18:45 +0530

    models fillable properties and reltionships added done

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Booking extends Model
 {
     use HasFactory;
+    
+    protected $table = 'bookings';
+
+    protected $fillable = [
+        'user_id',
+        'event_id',
+        'num_of_tickets'
+    ];
+    
+    public function user(): BelongsTo
+    {
+       return $this->belongsTo(User::class);
+    }
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
 }
