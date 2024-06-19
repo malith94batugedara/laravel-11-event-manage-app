d8a2201913e95bdf68125befee7b2cbf4a766761
malith94batugedara <malithbatugedara@gmail.com>
2024-06-19 10:51:35 +0530

    event store part done

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateEventRequest;
+use App\Models\Country;
+use App\Models\Event;
+use App\Models\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Illuminate\View\View;
 
 class EventController extends Controller
@@ -18,17 +24,31 @@ class EventController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create():View
     {
-        //
+        $countries = Country::all();
+        $tags = Tag::all();
+        return view('events.create',compact('countries','tags'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateEventRequest $request)
     {
-        //
+        if($request->hasFile('image')){
+            $data = $request->validated();
+
+            $data['image'] = Storage::putFile('events',$request->file('image'));
+            $data['user_id'] = auth()->id();
+            $data['slug'] = Str::slug($request->title);
+
+            Event::create($data);
+            return redirect(route('events.index'))->with('message','Event Added Successfully!');
+        }
+        else{
+            return back();
+        }
     }
 
     /**
--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,13 +1,24 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Events') }}
-        </h2>
+        <div class="flex justify-between">
+          <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+             {{ __('Events') }}
+          </h2>
+          <div>
+             <a href="{{ route('events.create') }}" class="dark:text-white hover:text-slate-200">New Event</a>
+          </div>
+        </div>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          
+             <div class="text-green-900 dark:text-green">
+                @if(session('message'))
+                  <div class="alert alert-success">
+                      {{ session('message') }}
+                  </div>
+                @endif
+             </div>
         </div>
     </div>
 </x-app-layout>
